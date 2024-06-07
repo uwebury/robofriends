79177635167544fc2fff8eed39a20cd36880b0e0
uwebury <142432719+uwebury@users.noreply.github.com>
2024-06-07 14:36:42 +0200

    Add key prop to mapping

--- src/CardList.js
+++ src/CardList.js
@@ -4,7 +4,12 @@ import Card from "./Card";
 const CardList = ({ robots }) => {
   const cardsArray = robots.map((user, i) => {
     return (
-      <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
+      <Card
+        key={i}
+        id={robots[i].id}
+        name={robots[i].name}
+        email={robots[i].email}
+      />
     );
   });
   return <div>{cardsArray}</div>;
