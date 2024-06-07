dccd60b9fd02e3aa7c83a7c147031351e33b55f9
uwebury <142432719+uwebury@users.noreply.github.com>
2024-06-07 13:48:28 +0200

    Update imports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Card from "./Card";
-import "tachyons/css/tachyons.min.css";
+import "tachyons";
+import robots from "./robots";
 
 ReactDOM.render(
   <div>
