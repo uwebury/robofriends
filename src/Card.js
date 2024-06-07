7b7596806e6ac8ca957a5ad90d4efff5b6445da1
uwebury <142432719+uwebury@users.noreply.github.com>
2024-06-07 13:40:26 +0200

    Create Card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <img alt="robot" src="https://robohash.org/testtest?200x200" />
+      <div>
+        <h2>Jane Doe</h2>
+        <p>jane.doe@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
