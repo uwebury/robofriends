6ce44bb75fc4d74d1bea16847754342f469f6dab
uwebury <142432719+uwebury@users.noreply.github.com>
2024-07-20 12:29:14 +0200

    Redux connect()

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
+import { Provider, connect } from "react-redux";
 import { createStore } from "redux";
 import "./index.css";
 import App from "./containers/App";
--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,10 @@
 import { CHANGE_SEARCH_FIELD } from "./constants";
 
-const initalState = {
+const initialStateSearch = {
   searchField: "",
 };
 
-export const searchRobots = (state = initalState, action = {}) => {
+export const searchRobots = (state = initialStateSearch, action = {}) => {
   switch (action.type) {
     case CHANGE_SEARCH_FIELD:
       return Object.assign({}, state, { searchField: action.payload });
