5f1ca105c8d651b02f241380537ab69d98001ad3
uwebury <142432719+uwebury@users.noreply.github.com>
2024-07-20 11:59:18 +0200

    Create Provider component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import "./index.css";
 import App from "./containers/App";
+import { searchRobots } from "./reducers";
 import "tachyons";
+
+const store = createStore(searchRobots);
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
