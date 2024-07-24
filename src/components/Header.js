5c3b1806d24deee2496118093819f870637b882d
uwebury <142432719+uwebury@users.noreply.github.com>
2024-07-24 12:03:43 +0200

    Create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  shouldComponentUpdate() {
+    return false;
+  }
+  render() {
+    return <h1 className="f1">RoboFriends</h1>;
+  }
+}
+
+export default Header;
